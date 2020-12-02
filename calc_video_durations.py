596ff53899e0b1d93ab5c2c7e5ed658719fc7f5c
Tim Kontiki <fr2kontiki@gmail.com>
2020-12-02 12:21:38 +0300

    Prefix output filename with total duration

--- calc_video_durations.py
+++ calc_video_durations.py
@@ -1,39 +1,29 @@
 import os
 import subprocess
-import glob 
-import os
+import glob
 
-def scan_for_files_recursivly(path=None):	
+# External library: pip install pymediainfo
+from pymediainfo import MediaInfo
+
+def scan_for_files_recursivly(path: str=None) -> 'dict of str':	
 	if not path:
-		path = os.getcwd()		
+		path = os.getcwd()
 		files = []
+		# Get all files recursively
 		for root, d, f in os.walk(path):		
 			for basename in f:
 				filename = os.path.join(root, basename)
 				files.append(filename)
 		return {'files': files}
 
-def scan_for_files(path=None):
-	if not path:
-		path = os.getcwd()
-		folders = []
-		files = []
-		for item in os.listdir(path):
-			if os.path.isdir(item):
-				folders.append(item)
-			else:
-				files.append(item)
-		return {'folders': folders, 'files': files}
-
-def get_duration(path):
-	from pymediainfo import MediaInfo
+def get_duration(path: str) -> float:
 	media_info = MediaInfo.parse(path)
 	#duration in milliseconds
 	duration_in_ms = media_info.tracks[0].duration
-	print(duration_in_ms)
+	# print(duration_in_ms)
 	return duration_in_ms
 
-def convert_milliseconds(ms=1000, to='minutes'):
+def convert_milliseconds(ms: int=1000, to: str='minutes') -> float:
 	''' Convert milliseconds to seconds/minutes/hours '''
 	conversion_dict = {'minutes': 1000 * 60,
 		'seconds': 1000,
@@ -41,12 +31,17 @@ def convert_milliseconds(ms=1000, to='minutes'):
 	}	
 	try:
 		converted = ms / conversion_dict.get(to)
-		print(f'{ms} milliseconds is equal to {round(converted, 5)} {to}')
+		# print(f'{ms} milliseconds is equal to {round(converted, 5)} {to}')
 		return converted
 	except TypeError:
 		return -1
 
-def scan_videos_durations(path=None, display=False, time_unit='minutes', export=False, export_name='video_durations.html'):
+def scan_videos_durations(path: str=None, display: bool=False, time_unit: str='minutes', 
+		export: bool=False, export_name: str='video_durations.html'
+	) -> dict:
+	''' Return a dict of path and duration 
+		Ex: {"path": 4} 
+	'''
 	durations = {}
 	for filename in scan_for_files_recursivly(path).get('files'):
 		if not filename:
@@ -54,6 +49,8 @@ def scan_videos_durations(path=None, display=False, time_unit='minutes', export=
 		durations[filename] = get_duration(filename)
 	if display:
 		[print(f'{filename} => {convert_milliseconds(duration, time_unit)}') for filename,duration in durations.items()]
+
+	# Only if we want to export as HTML file
 	if export:
 		html = '''<!DOCTYPE html>
 <html>
@@ -109,34 +106,58 @@ def scan_videos_durations(path=None, display=False, time_unit='minutes', export=
 					<td>{filename}</td>
 					<td>{formatted_duration} {time_unit}</td>
 				</tr>'''
+		# Calculate sum of videos duration in milliseconds
 		sum_durations = round(
 			convert_milliseconds(
 				sum([x for x in durations.values() if x]),
 				time_unit
 			), 4
 		)
+		# Convert durations according to time unit (minutes or hour)
 		if time_unit == 'minutes':
 			total = round(sum_durations / 60)
 		else:
 			total = round(sum_durations * 60)
+		# Add row total to HTML export file
 		html += f'''
 			<tr>
 				<td class="table__total">Total</td>
 				<td class="table__total">{sum_durations} {time_unit} | {total}</td>
 			</tr>'''
 		html += '</table></body></html>'
-		with open(export_name, 'w') as f:
+		# Save to a local .html file
+		if total <= 0:
+			print('Total can not be 0')
+			return
+		with open(f'{total}-{export_name}', 'w') as f:			
 			f.write(html)
+			print(f'Saved to: {total}-{export_name}')
 	return durations
 
 def get_length(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                             "format=duration", "-of",
-                             "default=noprint_wrappers=1:nokey=1", filename],
+			"format=duration", "-of",
+			"default=noprint_wrappers=1:nokey=1", filename
+		],
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
+        stderr=subprocess.STDOUT
+    )
     return float(result.stdout)
 
+def scan_for_files_and_folders(path: str=None) -> dict:
+	if not path:
+		path = os.getcwd()
+		folders = []
+		files = []
+		for item in os.listdir(path):
+			if os.path.isdir(item):
+				folders.append(item)
+			else:
+				files.append(item)
+		return {'folders': folders, 'files': files}
+
 if __name__ == '__main__':	
-	dur = scan_videos_durations(display=True, time_unit='minutes', export=True)
-	
+	dur = scan_videos_durations(display=False, time_unit='minutes', export=True)
+	# print(type(dur), dur)
+	# print(scan_for_files_recursivly())
+	# print(scan_for_files_and_folders())	
---
+++ lenvid.bat
@@ -0,0 +1,2 @@
+C:\Users\Usera\AppData\Local\Programs\Python\Python36-32\python.exe "D:\laboratoire\repo\calc_videos_durations\calc_video_durations.py"
+pause
